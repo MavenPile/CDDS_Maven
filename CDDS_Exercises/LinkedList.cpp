34feb903cd733dd9c655b0704e99572538c64293
MavenPile <s214290@students.aie.edu.au>
2024-05-30 16:29:55 +0930

    Attempting implementation of Iterators in LinkedList
    -Implementing iterators
    -Lots of errors still exist, and iterators are not complete

--- CDDS_Exercises/LinkedList.cpp
+++ CDDS_Exercises/LinkedList.cpp
@@ -1,91 +1,199 @@
 #include "LinkedList.h"
-#include "Node.h"
 
-LinkedList::LinkedList()
+//	Node METHODS
+
+template<typename T>
+LinkedList<T>::Node::Node()
+{
+	m_data = nullptr, m_previous = nullptr, m_next = nullptr;
+}
+
+template<typename T>
+LinkedList<T>::Node::Node(T value)
+{
+	m_data = value, m_previous = nullptr, m_next = nullptr;
+}
+
+
+//	Iterator METHODS
+
+template<typename T>
+LinkedList<T>::Iterator::Iterator()
+{
+	m_currentNode = nullptr;
+}
+
+template<typename T>
+LinkedList<T>::Iterator::Iterator(Node& node)
+{
+	m_currentNode = node;
+}
+
+template<typename T>
+void LinkedList<T>::Iterator::SetNode(Node& newNode)
+{
+	m_currentNode = newNode;
+}
+
+template<typename T>
+void LinkedList<T>::Iterator::SetData(T newData)
+{
+	m_currentNode->m_data = newData;
+}
+
+template<typename T>
+LinkedList<T>::Node LinkedList<T>::Iterator::GetNode()
+{
+	return m_currentNode;
+}
+
+template<typename T>
+T LinkedList<T>::Iterator::GetData()
+{
+	return m_currentNode->m_data;
+}
+
+template<typename T>
+T LinkedList<T>::Iterator::Current()
+{
+	return m_currentNode->m_data;
+}
+
+template<typename T>
+T LinkedList<T>::Iterator::operator*()
+{
+	return m_currentNode->m_data;
+}
+
+template<typename T>
+void LinkedList<T>::Iterator::MoveNext()
+{
+	m_currentNode = m_currentNode->m_next;
+}
+
+template<typename T>
+void LinkedList<T>::Iterator::operator ++ (int i)
+{
+	m_currentNode = m_currentNode->m_next;
+}
+
+template<typename T>
+void LinkedList<T>::Iterator::MovePrev()
+{
+	m_currentNode = m_currentNode->m_previous;
+}
+
+template<typename T>
+void LinkedList<T>::Iterator::operator -- (int i)
+{
+	m_currentNode = m_currentNode->m_previous;
+}
+
+//	LinkedList METHODS
+
+template<typename T>
+LinkedList<T>::LinkedList()
 {
 	m_head = nullptr;
 	m_tail = nullptr;
 }
 
-LinkedList::~LinkedList()
+template<typename T>
+LinkedList<T>::~LinkedList()
 {
 
 }
 
-void LinkedList::PushFront(int value)
+template<typename T>
+void LinkedList<T>::PushFront(T value)
 {
 	Node newNode(value);
 	newNode.m_next = m_head;
 	m_head->m_previous = &newNode;
-	m_head = &newNode;
+	m_head = Iterator(newNode);
 }
 
-void LinkedList::PushBack(int value)
+template<typename T>
+void LinkedList<T>::PushBack(T value)
 {
 	Node newNode(value);
 	newNode.m_previous = m_tail;
 	m_tail->m_next = &newNode;
-	m_tail = &newNode;
+	m_tail = Iterator(newNode);
 }
 
-void LinkedList::InsertAfter(Node* iterator, int value)
+template<typename T>
+void LinkedList<T>::InsertAfter(Iterator* iterator, T value)
 {
 	Node newNode(value);
 
-	iterator->m_next->m_previous = &newNode;
+	iterator->m_currentNode->m_next->m_previous = &newNode;
 	newNode.m_next = iterator->m_next->m_previous;
 
 	newNode.m_previous = iterator;
 	iterator->m_next = &newNode;
 }
 
-Node* LinkedList::Begin()
+template<typename T>
+LinkedList<T>::Iterator LinkedList<T>::Begin()
 {
 	return m_head;
 }
 
-Node* LinkedList::End()
+template<typename T>
+LinkedList<T>::Iterator LinkedList<T>::End()
 {
 	return m_tail;
 }
 
-int LinkedList::First()
+template<typename T>
+T LinkedList<T>::First()
 {
 	return m_head->m_data;
 }
 
-int LinkedList::Last()
+template<typename T>
+T LinkedList<T>::Last()
 {
 	return m_tail->m_data;
 }
 
-int LinkedList::Count()
+template<typename T>
+int LinkedList<T>::Count()
 {
-	return 0;
+	int count = 0;
+
+	return p_Counting(m_head, count);
 }
 
-int* LinkedList::Counting(Node* iterator, int* count)
-{
+template<typename T>
+int LinkedList<T>::p_Counting(Iterator& iterator, int& count)
+{	
 	count += 1;
 	
 	if (iterator->m_next != nullptr)
 	{
-
+		p_Counting(iterator->m_next, count);
 	}
+
+	return count;
 }
 
-void LinkedList::Erase(Node* iterator)
+template<typename T>
+void LinkedList<T>::Erase(Iterator* iterator)
 {
 	iterator->m_previous = iterator->m_next;
 	iterator->m_next = iterator->m_previous;
 }
 
-void LinkedList::Remove(int value)
+template<typename T>
+void LinkedList<T>::Remove(T value)
 {
-	RemoveNext(*m_head, value);
+	p_RemoveNext(*m_head, value);
 }
 
-void LinkedList::RemoveNext(Node& iterator, int value)
+template<typename T>
+void LinkedList<T>::p_RemoveNext(Iterator& iterator, T value)
 {
 	Node next = *iterator.m_next;
 	
@@ -94,22 +202,25 @@ void LinkedList::RemoveNext(Node& iterator, int value)
 		Erase(&iterator);
 	}
 
-	RemoveNext(next, value);
+	p_RemoveNext(next, value);
 }
 
-void LinkedList::PopBack()
+template<typename T>
+void LinkedList<T>::PopBack()
 {
 	m_tail->m_previous->m_next = nullptr;
 }
 
-void LinkedList::PopFront()
+template<typename T>
+void LinkedList<T>::PopFront()
 {
 	m_head->m_next->m_previous = nullptr;
 }
 
-bool LinkedList::IsEmpty()
+template<typename T>
+bool LinkedList<T>::IsEmpty()
 {
-	if (CheckEmpty(*m_head->m_next))
+	if (p_CheckEmpty(*m_head->m_next))
 	{
 		return false;
 	}
@@ -117,19 +228,21 @@ bool LinkedList::IsEmpty()
 	return true;
 }
 
-bool LinkedList::CheckEmpty(Node& iterator)
+template<typename T>
+bool LinkedList<T>::p_CheckEmpty(Iterator& iterator)
 {
-	if (&iterator.m_data != nullptr)
+	if (&iterator.GetNode().m_data != nullptr)
 	{
 		return true;
 	}
-	else if (CheckEmpty(*iterator.m_next))
+	else if (p_CheckEmpty(*iterator.m_next))
 	{
 		return false;
 	}
 }
 
-void LinkedList::Clear()
+template<typename T>
+void LinkedList<T>::Clear()
 {
 	m_head = nullptr;
 	m_tail = nullptr;
