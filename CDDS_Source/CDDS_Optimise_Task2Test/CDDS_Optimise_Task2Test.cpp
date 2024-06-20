0718e7c9fbcfa42992724c3d0ad3be0452bf4491
MavenPile <s214290@students.aie.edu.au>
2024-06-20 16:17:43 +0930

    Starting Test Function writing for task 2
    -Not really many resources for it

---
+++ CDDS_Source/CDDS_Optimise_Task2Test/CDDS_Optimise_Task2Test.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CDDSOptimiseTask2Test
+{
+	TEST_CLASS(CDDSOptimiseTask2Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+		TEST_METHOD(TestMethod2)
+		{
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+
+		}
+	};
+}
