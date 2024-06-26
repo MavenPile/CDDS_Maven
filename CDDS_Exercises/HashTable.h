67701b9c1f23824fb810311af06f9a11ec641365
MavenPile <s214290@students.aie.edu.au>
2024-06-26 10:21:09 +0930

    Just some stuff

--- CDDS_Exercises/HashTable.h
+++ CDDS_Exercises/HashTable.h
@@ -5,65 +5,26 @@ class string;
 template<typename T>
 class HashTable
 {
-public:
-
-	//	CONSTRUCTORS
-	HashTable(unsigned int size);
-	~HashTable();
-
-	//	METHODS
-	void Add(T& item, const string& key);
-	void Remove(const string& key);
-
-	void Clear(const string& key);
-
-	//	OPERATORS
-	T& operator [] (const string& key);
-	const T& operator [] (const string& key) const;
-
-	struct Bucket
-	{
-		//	VARIABLES
-		T* m_contents;
-		int m_size;
-		int m_index;
-
-		//	CONSTRUCTORS
-		Bucket() { m_contents = new T[2], m_size = 2, m_index = -1; }
-		~Bucket() { delete[] m_contents; }
-
-		//	METHODS
-		void Add(T& item, unsigned int& key) {
-			if (m_index = m_size) {	//	if at max bucket size
-				T* temp = new T[m_size + 1];	//	increase bucket size by 1
-
-				for (int i = 0; i < m_size; i++) {
-					temp[i] = m_contents[i];
-				}
-
-				delete[] m_contents;
-				m_contents = temp;
-			}
-			
-			m_index++;
-			m_contents[key % m_size] = item;	//	add new item to bucket
-		}
-
-		void Remove() {
-
-		}
-
-		T& Query() {
-
-		}
-	};
-
 private:
-
 	unsigned int HashFunction(const char* key);
+	const unsigned int HashFunction(const char* key) const;
 
 	T* m_data;
 	unsigned int m_size;
+
+public:
+	HashTable();	//	unresolved external symbol error
+	HashTable(unsigned int size);
+	~HashTable();	//	unresolved external symbol error
+
+	void Add(const T& item, const char* key);	//	unresolved external symbol error
+	void Remove(const char* key);
+	void Clear();
+
+	T& DataAt(const char* key);
+	const T& DataAt(const char* key) const;
+	T& operator [] (const char* key);	//	unresolved external symbol error
+	const T& operator [] (const char* key) const;
 };
 
 
